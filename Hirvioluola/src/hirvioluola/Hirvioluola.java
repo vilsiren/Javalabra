17e66d405c0ac11ba52937e8ec075a3144f8bf3b
unknown <vilsiren@cs.helsinki.fi>
2012-12-17 19:53:52 +0200

    hirvioluola-kansio lisatty

---
+++ Hirvioluola/src/hirvioluola/Hirvioluola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hirvioluola;
+
+/**
+ *
+ * @author Ville
+ */
+public class Hirvioluola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
